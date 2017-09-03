593f492dd960735ad5c7a6b1a3c8657e4830e9e8
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-03 02:35:27 -0600

    Initial test of who_am_i function using Harvest::ApiClient.

---
+++ lib/harvest/api_client.rb
@@ -0,0 +1,76 @@
+module Harvest
+  class ApiClient
+    #This class has been based on:
+    # https://github.com/harvesthq/harvest_api_samples/blob/master/harvest_api_sample.rb
+    HAS_SSL          = true
+    def initialize(organization)
+      @organization = organization
+      @preferred_protocols = [HAS_SSL, ! HAS_SSL]
+      connect!
+    end 
+    def who_am_i(orzanization)
+      request('/account/who_am_i', :get)
+    end   
+
+    def headers
+      {
+        "Accept"        => "application/json",
+        "Content-Type"  => "application/json; charset=utf-8",
+        "Authorization" => "Basic #{auth_string}",
+        "User-Agent"    => 'API client Ruben' 
+      }
+    end
+
+    def auth_string
+      Base64.encode64("#{@organization.username}:#{@organization.password}").delete("\r\n")
+    end
+
+    def send_request path, method = :get, body = ''
+      case method
+      when :get
+        @connection.get(path, headers)
+      end
+    end
+
+    private 
+    def connect!
+      port = has_ssl ? 443 : 80
+      @connection = Net::HTTP.new("#{@organization.subdomain}.harvestapp.com", port)
+      @connection.use_ssl     = has_ssl
+      @connection.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+   
+    def has_ssl
+      @preferred_protocols.first
+    end 
+
+    def request path, method = :get, body = ""
+      response = send_request( path, method, body)
+      if response.class < Net::HTTPSuccess
+        on_completed_request
+        return response
+      elsif response.class == Net::HTTPServiceUnavailable
+        raise "Got HTTP 503 three times in a row" if retry_counter > 3
+        sleep(response['Retry-After'].to_i + 5)
+        request(path, method, body)
+      elsif response.class == Net::HTTPFound
+        @preferred_protocols.shift
+        raise "Failed connection using http or https" if @preferred_protocols.empty?
+        connect!
+        request(path, method, body)
+      else
+        dump_headers = response.to_hash.map { |h,v| [h.upcase,v].join(': ') }.join("\n")
+        raise "#{response.message} (#{response.code})\n\n#{dump_headers}\n\n#{response.body}\n"
+      end
+    end
+
+    def on_completed_request
+      @retry_counter = 0
+    end
+
+    def retry_counter
+      @retry_counter ||= 0
+      @retry_counter += 1
+    end
+  end
+end
---
+++ test/unit/harvest_api_client_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+require 'harvest/organization'
+require 'harvest/api_client'
+require 'json'
+
+class HarvestApiClientTest < ActiveSupport::TestCase
+  def setup
+    @username = 'ruben.amortegui@gmail.com'
+    @password = 'publico01'
+    @subdomain = 'ramortegui'
+    @organization = Harvest::Organization.new(@username, @password, @subdomain) 
+  end
+
+  test 'Get connection to the harvestapp' do
+    api_client = Harvest::ApiClient.new(@organization)
+    who_am_i = api_client.who_am_i(@organization)
+    assert_equal(@username, JSON.parse(who_am_i.body)["user"]["email"] , "Check success who am I.") 
+  end
+
+end
