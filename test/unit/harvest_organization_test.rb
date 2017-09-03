9444cd85b15cae4542308329228804eae54f23e7
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-02 23:59:10 -0600

    Refactor username and password of an organization

--- test/unit/harvest_organization_test.rb
+++ test/unit/harvest_organization_test.rb
@@ -2,16 +2,18 @@ require 'test_helper'
 require 'harvest/organization'
 
 class HarvestOrganizationTest < ActiveSupport::TestCase
+  @username = 'username'
+  @password = 'password'
   setup do
-    @organization = Harvest::Organization.new('username','password')
+    @organization = Harvest::Organization.new(@username, @passsword)
   end
 
   test "Create a new Organization" do
-    assert_equal(Harvest::Organization,@organization.class,"Create a new organization")
+    assert_equal(Harvest::Organization, @organization.class, "Create a new organization")
   end
 
   test "Organization needs to have a email and password" do
-    assert(Harvest::Organization.new('username','password'),
+    assert(Harvest::Organization.new(@username, @password),
            "New organization with minimum params")
   end
 
