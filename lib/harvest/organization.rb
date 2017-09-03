cc0c2a7fce1e3910421060aef3ab166b5db514e1
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-02 23:42:53 -0600

    Add parameters to create an Harvest::Organization module

--- lib/harvest/organization.rb
+++ lib/harvest/organization.rb
@@ -1,5 +1,8 @@
 module Harvest
   class Organization
-
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
   end 
 end
--- test/unit/harvest_organization_test.rb
+++ test/unit/harvest_organization_test.rb
@@ -3,11 +3,16 @@ require 'harvest/organization'
 
 class HarvestOrganizationTest < ActiveSupport::TestCase
   setup do
-    @organization = Harvest::Organization.new()
+    @organization = Harvest::Organization.new('username','password')
   end
 
   test "Create a new Organization" do
     assert_equal(Harvest::Organization,@organization.class,"Create a new organization")
+  end
+
+  test "Organization needs to have a email and password" do
+    assert(Harvest::Organization.new('username','password'),
+           "New organization with minimum params")
   end
 
 end
