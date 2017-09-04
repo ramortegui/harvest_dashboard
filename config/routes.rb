074ff8b5ef4aff24f00df57cd5996a5ba49b8a56
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-04 01:05:58 -0600

    Add dashboard#index as the root of the app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   resources :organizations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root :to => "dashboard#index" 
 end
