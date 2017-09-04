cbe950a6068e65a529695070b6a090d1c5529c34
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-04 17:29:48 -0600

    Move detailed report funcitonality to Organization model.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,5 @@
 class DashboardController < ApplicationController
-  require 'harvest'
   def index
-    organizations = Organization.all.map{ |org|
-      organization = Harvest::Organization.new(org.username, org.password, org.subdomain)
-    }
-    @detailed_report = []
-    if( organizations.count > 0 )
-      report = Harvest::Report.new( organizations, '20160101', '20170101')
-      @detailed_report = report.get_detailed_report
-    end
+    @detailed_report = Organization.detailed_report
   end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,16 @@
 class Organization < ApplicationRecord
   validates :username, :password, :subdomain, presence: true 
+
+  def self.detailed_report
+    require 'harvest'
+    @detailed_report = []
+    organizations = Organization.all.map{ |org|
+      organization = Harvest::Organization.new(org.username, org.password, org.subdomain)
+    }
+    if( organizations.count > 0 )
+      report = Harvest::Report.new( organizations, '20160101', '20170101')
+      @detailed_report = report.get_detailed_report
+    end
+    @detailed_report
+  end
 end
