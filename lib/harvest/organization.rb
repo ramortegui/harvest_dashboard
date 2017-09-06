adef3a548a3f2e4ae7da0c941e4455bb84e61194
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-05 22:41:07 -0600

    Add documentation to Harvest::Organization.

--- lib/harvest/organization.rb
+++ lib/harvest/organization.rb
@@ -1,6 +1,8 @@
 module Harvest
+  # Object used as an abstraction of the harvestapp account
   class Organization
     attr_reader :username, :password, :subdomain
+    #Receive at lease the useranme, password, and the subdomain of the account.
     def initialize(username, password, subdomain)
       @username = username
       @password = password
