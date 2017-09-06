361d2e85300bca5220411aa563c790c784e9bb23
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-06 00:12:35 -0600

    Add Date filters.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,20 @@
 class DashboardController < ApplicationController
   def index
-    from = params[:from] ? Date.parse(params[:from]).strftime('%Y%m%d') : DateTime.now.to_date.beginning_of_month.strftime('%Y%m%d')
-    to = params[:to] ? Date.parse(params[:to]).strftime('%Y%m%d') : DateTime.now.to_date.end_of_month.strftime('%Y%m%d')
-    @detailed_report = Organization.detailed_report( from, to)
+    if( params[:from] )
+      @from = Date.parse(params[:from]).strftime('%Y%m%d') 
+      @from_show = params[:from]
+    else
+      @from = DateTime.now.to_date.beginning_of_month.strftime('%Y%m%d')
+      @from_show = DateTime.now.to_date.beginning_of_month.strftime('%d-%m-%Y')
+    end
+
+    if( params[:to] ) 
+      @to = Date.parse(params[:to]).strftime('%Y%m%d')
+      @to_show = params[:to] 
+    else
+      @to = DateTime.now.to_date.end_of_month.strftime('%Y%m%d')
+      @to_show = DateTime.now.to_date.end_of_month.strftime('%d-%m-%Y')
+    end
+    @detailed_report = Organization.detailed_report( @from, @to)
   end
 end
