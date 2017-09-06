1cddf10b6bab49b9d0d843869956bec3cd4dd1c3
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-05 23:37:55 -0600

    Add date filters to harvers dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @detailed_report = Organization.detailed_report
+    from = params[:from] ? Date.parse(params[:from]).strftime('%Y%m%d') : DateTime.now.to_date.beginning_of_month.strftime('%Y%m%d')
+    to = params[:to] ? Date.parse(params[:to]).strftime('%Y%m%d') : DateTime.now.to_date.end_of_month.strftime('%Y%m%d')
+    @detailed_report = Organization.detailed_report( from, to)
   end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,14 +2,14 @@ class Organization < ApplicationRecord
   validates :username, :password, :subdomain, presence: true 
   validates_with OrganizationValidator
 
-  def self.detailed_report
+  def self.detailed_report(from,to)
     require 'harvest'
     @detailed_report = []
     organizations = Organization.all.map{ |org|
       organization = Harvest::Organization.new(org.username, org.password, org.subdomain)
     }
     if( organizations.count > 0 )
-      report = Harvest::Report.new( organizations, '20160101', '20170101')
+      report = Harvest::Report.new( organizations, from, to)
       @detailed_report = report.get_detailed_report
     end
     @detailed_report
