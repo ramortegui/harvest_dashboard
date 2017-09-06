fa7181702249265594357afcb0eae81ef06cb8b7
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-06 08:24:55 -0600

    Add flash message in case of error.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    #Define and translate dates
     if( params[:from] )
       @from = Date.parse(params[:from]).strftime('%Y%m%d') 
       @from_show = params[:from]
@@ -10,11 +11,18 @@ class DashboardController < ApplicationController
 
     if( params[:to] ) 
       @to = Date.parse(params[:to]).strftime('%Y%m%d')
-      @to_show = params[:to] 
+      @to_show = params[:to]
     else
       @to = DateTime.now.to_date.end_of_month.strftime('%Y%m%d')
       @to_show = DateTime.now.to_date.end_of_month.strftime('%d-%m-%Y')
     end
-    @detailed_report = Organization.detailed_report( @from, @to)
+
+    #Check exceptions running detailed report
+    begin
+      @detailed_report = Organization.detailed_report( @from, @to)
+    rescue Exception => e
+      @detailed_report = []
+      flash.now[:error] =  e.message
+    end
   end
 end
